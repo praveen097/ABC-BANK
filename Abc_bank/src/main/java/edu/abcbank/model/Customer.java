e4f2c3fed9c015292ad62cb7a3807a8c88a1a8ca
Praveen Kumar Guttapalyam <43851410+praveen097@users.noreply.github.com>
2022-05-07 14:25:53 -0700

    Added customer and account class details

--- Abc_bank/src/main/java/edu/abcbank/model/Customer.java
+++ Abc_bank/src/main/java/edu/abcbank/model/Customer.java
@@ -1,5 +1,118 @@
 package edu.abcbank.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "abc_customer")
 public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int customerId;
+	@Column(length = 30, nullable = false)
+	private String firstName;
+	@Column(length = 30, nullable = true)
+	private String lastName;
+	@Column(length = 30, nullable = false)
+	private String email;
+	@Column(length = 10, nullable = false)
+	private long phone;
+	@Column(length = 45, nullable = false)
+	private String address;
+	@Column(length = 20, nullable = false)
+	private String city;
+	@Column(length = 20, nullable = false)
+	private String state;
+	@Column(nullable = false)
+	private int pinCode;
+	@Temporal(TemporalType.DATE)
+	@Column(nullable = false)
+	private Date dateOfBirth;
+	@Column(length = 12, nullable = false)
+	private String gender;
+	@Column(length = 10, nullable = false)
+	private String userType;
+	
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getPinCode() {
+		return pinCode;
+	}
+	public void setPinCode(int pinCode) {
+		this.pinCode = pinCode;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+		
 
 }
