b555d4e0e466c9cf69f46339e225099f59306753
Praveen Kumar Guttapalyam <43851410+praveen097@users.noreply.github.com>
2022-05-06 18:05:14 -0700

    Added initial project files
    Added initial files

---
+++ Abc_bank/src/main/java/edu/abcbank/main/AbcBankApplication.java
@@ -0,0 +1,13 @@
+package edu.abcbank.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcBankApplication.class, args);
+	}
+
+}
